a7a35b7c19eedd41a8911a0bbcc81bac3e03365d
Fenn <pokefenn@googlemail.com>
2014-09-15 11:14:40 +0100

    WIP left click chisel stuff
    commit changed by EoD to remove unfinished code

--- src/main/java/info/jbcs/minecraft/chisel/item/ItemChisel.java
+++ src/main/java/info/jbcs/minecraft/chisel/item/ItemChisel.java
@@ -1,26 +1,14 @@
 package info.jbcs.minecraft.chisel.item;
 
-import cpw.mods.fml.common.FMLCommonHandler;
 import info.jbcs.minecraft.chisel.Chisel;
-import info.jbcs.minecraft.chisel.ChiselBlocks;
-import info.jbcs.minecraft.chisel.Configurations;
 import info.jbcs.minecraft.chisel.carving.CarvableHelper;
 import info.jbcs.minecraft.chisel.carving.Carving;
-import info.jbcs.minecraft.chisel.carving.CarvingVariation;
-import info.jbcs.minecraft.chisel.client.GeneralChiselClient;
-import net.minecraft.block.Block;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemTool;
 import net.minecraft.world.World;
-import net.minecraftforge.common.ForgeHooks;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Random;
 import java.util.Set;
 
 public class ItemChisel extends ItemTool
@@ -56,7 +44,9 @@ public class ItemChisel extends ItemTool
     }
 
     @Override
-    public boolean onBlockStartBreak(ItemStack itemstack, int X, int Y, int Z, EntityPlayer player) {
+    public boolean onBlockStartBreak(ItemStack itemstack, int X, int Y, int Z, EntityPlayer player)
+    {
         return true;
     }
+
 }
