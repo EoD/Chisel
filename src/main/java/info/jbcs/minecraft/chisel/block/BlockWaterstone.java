bea46633351c11a44b0d76f0f6316552c7bb579b
Solestis <shunthakin@msn.com>
2014-09-29 20:03:37 +0200

    Added Waterstone
    Added:
    New class for Waterstone, so particle effects may be implemented later.
    Code to add Waterstone.
    Crafting recipe for Waterstone.
    Modified lavastone textures for Waterstone and any other fluid blocks in
    the future.
    English language entry for Waterstone.

---
+++ src/main/java/info/jbcs/minecraft/chisel/block/BlockWaterstone.java
@@ -0,0 +1,19 @@
+package info.jbcs.minecraft.chisel.block;
+
+
+import net.minecraft.block.material.Material;
+
+public class BlockWaterstone extends BlockMarbleTexturedOre
+{
+    public BlockWaterstone(Material mat, String baseIcon)
+    {
+        super(mat, baseIcon);
+    }
+
+    @Override
+    public boolean renderAsNormalBlock()
+    {
+        return true;
+    }
+
+}
